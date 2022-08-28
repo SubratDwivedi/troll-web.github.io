d244a1a3ebffb688fda91ec6d7270f43d642492d
Subrat Dwivedi <96533293+SubratDwivedi@users.noreply.github.com>
2022-08-29 00:21:03 +0530

    Add files via upload

---
+++ troll_web/app.js
@@ -0,0 +1,26 @@
+
+let txtx = document.getElementById('fine');
+
+let txtxtx = document.getElementById('bad');
+
+const img1 = document.getElementById('images1');
+
+function fine() {
+  txtx.innerText = 'That\'s Great'
+}
+
+function bad() {
+  txtxtx.innerText = 'Well That Sucks'
+}
+
+
+//let gift = document.getElementById('gift');
+
+//let toggle = true;
+
+//gift.addEventListener('click', function() {
+//  toggle = !toggle;
+ // if (toggle) {
+//    gift.src = 'img222.png' }
+ // else {
+ //   gift.src = 'Luffy.png'}})
